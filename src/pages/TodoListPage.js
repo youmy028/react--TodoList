be5fbd6a01b14cc8faa7a6155fc14edba8f61088
youmy028 <youmy028@naver.com>
2023-02-21 15:40:29 +0900

    나머지 정렬 구현

--- src/pages/TodoListPage.js
+++ src/pages/TodoListPage.js
@@ -23,6 +23,28 @@ export default function TodoList() {
     return todosStatus.todos;
   };
   const filteredTodos = getFilteredTodos();
+  const getSortedTodos = () => {
+    if (sortIndex == 0) {
+      return [...filteredTodos].sort((a, b) => {
+        if (a.performDate == b.performDate) return 0;
+        return a.performDate > b.performDate ? 1 : -1;
+      });
+    } else if (sortIndex == 1) {
+      return [...filteredTodos].sort((a, b) => {
+        if (a.performDate == b.performDate) return 0;
+
+        return a.performDate < b.performDate ? 1 : -1;
+      });
+    } else if (sortIndex == 2) {
+      return [...filteredTodos].sort((a, b) => {
+        return a.id > b.id ? 1 : -1;
+      });
+    }
+
+    return filteredTodos;
+  };
+
+  const sortedTodos = getSortedTodos();
   return (
     <>
       <TodoOptionDrawer status={todoOptionDrawerStatus} />
@@ -116,7 +138,7 @@ export default function TodoList() {
 
       <div className="mt-4 px-4">
         <ul>
-          {filteredTodos.map((todo, index) => (
+          {sortedTodos.map((todo, index) => (
             <TodoListItem
               key={todo.id}
               todo={todo}
